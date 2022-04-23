40a3afabeeadece9bb1a40ea435c2e4aeeac2e93
philip72 <philipwamai85@gmail.com>
2022-04-23 22:45:32 +0300

    base to it

--- app/src/main/java/com/example/ships/MainActivity.java
+++ app/src/main/java/com/example/ships/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.ships;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        finishActivity();
+
+    }
+    public boolean  onCreateOptionMenu(Menu filter){
+        getMenuInflater().inflate(R.menu.filter, filter);
+        MenuItem item = filter.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(filter);
     }
 }
---
+++ app/src/main/java/com/example/ships/ShipModal.java
@@ -0,0 +1,4 @@
+package com.example.ships;
+
+public class ShipModal {
+}
